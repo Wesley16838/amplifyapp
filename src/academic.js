0ff319ff19031fd64a3f89b71d6b2b1e3f84b539
WesleyWong <wesley16838@gmail.com>
2020-03-30 23:34:43 -0400

    update

--- src/academic.js
+++ src/academic.js
@@ -6,6 +6,7 @@ import ios from './Assets/img/ios2x.png'
 import bookstore from './Assets/img/bookstore2x.png'
 import tokyo from './Assets/img/tokyo2x.png'
 import vatm from './Assets/img/vatm2x.png'
+import hearttrade from './Assets/img/hearttrade.png'
 class Academic extends Component {
   constructor(props){
     super(props);
@@ -21,9 +22,15 @@ class Academic extends Component {
       <div className='academic'>
         <h1 data-aos="fade-in">Academic Projects</h1>
         <div className="academic-section">
+        <Academicpart title='HeartTrade' intro='•	A platform that increases efficiency of trading daily necessities with users near around current location' school='COVID-19 Global Hackathon' location='Devpost	' date='Mar. 2020' image={[hearttrade]} content='The COVID-19 Global Hackathon is an opportunity for developers to build software solutions that drive social impact, with the aim of tackling some of the challenges related to the current coronavirus (COVID-19) pandemic. Contributions include but not limited to UI Design and Front-End development using React Hooks and Context API' git='https://github.com/Wesley16838/hackathon-COVID19' url='https://hearttrade.herokuapp.com/' front='React.js/ React Hooks/ Context API/ SCSS/ Gulp/ google-map-react' back='Node.js/ Express.js/ RESTful API/ MongoDB/ MongoDB Atlas/ Firebase' mining=''/>
         <Academicpart title='PriCov' intro='Data Visualization Price Comparison Website' school='Stevens Institute of Technology' location='Hoboken, NJ' date='Fall 2019' image={[pricov]} content='Allows users to search products from eBay and Amazon using data 
 mining and visualizing  data as different charts' git='https://github.com/Wesley16838/PriCov' url='https://pricovfrontend.herokuapp.com/
 ' front='React.js/ Apollo/ SCSS/ Gulp/ d3.js' back='GraphQL/ MongoDB/ Atlas/ Heroku/ Firebase' mining='Python/ Selenium'/>
+        
+        <Academicpart title='Wesley Bookstore' intro='The bookstore using PHP implement basic features of online bookstore' school='Highline community college' location='Des Moines, WA' date='Winter 2018' image={[bookstore]} content='The bookstore is an academic project.  Using PHP connect to the 
+MySQL database phpmyadmin to implement basic features include 
+show book’s detail in the page, user sign up, login, add and remove 
+item to/ from the Cart. ' git='https://github.com/Wesley16838/Bookstore_PHP' url='http://weihsuanwong.com/Wesley_Bookstore/homepage/landingpage.php' front='HTML/CSS/Javascript/Bootstrap' back='PHP / SQL / phpMyAdmin' mining=''/>
         <Academicpart title='Walker Bookstore' intro='A RWD online bookstore - iOS App &amp; Web' school='Stevens Institute of Technology' location='Hoboken, NJ' date='Spring 2019' image={[responsive,ios]} content='Features includes sign up, log in, Cart(add,
  remove, update and place order), Search 
 books and display books in the page. This 
@@ -31,15 +38,11 @@ project cover security using JWT to
 authorize user’s information. Amazing 
 design and perfect error handle.' git='https://github.com/Wesley16838/Online-Bookstore' url='https://bookstore-cs546.appspot.com/
 ' front='Handlebars/ HTML/ CSS' back='RESTful API/ Express/ MongoDB/ Atlas/ JWT' mining=''/>
-        <Academicpart title='Wesley Bookstore' intro='The bookstore iusing PHP implement basic features of online bookstore' school='Highline community college' location='Des Moines, WA' date='Winter 2018' image={[bookstore]} content='The bookstore is an academic project.  Using PHP connect to the 
-MySQL database phpmyadmin to implement basic features include 
-show book’s detail in the page, user sign up, login, add and remove 
-item to/ from the Cart. ' git='https://github.com/Wesley16838/Bookstore_PHP' url='http://weihsuanwong.com/Wesley_Bookstore/homepage/landingpage.php' front='HTML/CSS/Javascript/Bootstrap' back='PHP / SQL / phpMyAdmin' mining=''/>
         <Academicpart title='Travel Guide' intro='A RWD static website with brief introduction about the Tokyo' school='Highline community college ' location='Des Moines' date=' Fall 2017' image={[tokyo]} content='Implemented RWD with pure CSS. A static webpage deployed in 
 godaddy. Introduction of some famous tourist attractions in Tokyo
 And how to preparefor trip.' git='https://github.com/Wesley16838/Travel_Guide' url='http://weihsuanwong.com/Travel_Guide/index.php' front='HTML/ CSS/ Javascript' back='' mining=''/>
         <Academicpart title='VATM' intro='Allows people to remotely retrieve ordered cash unlike a traditional ATM' school='Highline community college' location='Des Moines' date='Spring 2018' image={[vatm]} content='Vatm is going to save users time to get the cash back. Users order some food by using app and link to vatm when they check out. Users can choose the amount of cash back they prefer. Cash back will be delivered with food then.' git='https://github.com/Wesley16838/android_vatm' url='' front='Android Studio/ JAVA/ XML' back='Node.js/ RESTful API/ SQL/ phpMyAdmin' mining=''/>
-
+        
         </div>
 
       </div>
--- src/professional.js
+++ src/professional.js
@@ -4,7 +4,7 @@ import advantech from './Assets/img/advantech2x.png'
 import creatop from './Assets/img/creatop_lg@2x.png'
 import dct from './Assets/img/dct2x.png'
 import ispts from './Assets/img/ispts2x.png'
-import cannasphere from './Assets/img/cannasphere2x.png'
+
 class Professional extends Component {
   constructor(props){
     super(props);
@@ -18,7 +18,7 @@ class Professional extends Component {
   render() {
     return(
       <div className="professional">
-          <h1 data-aos="fade-in">Professional Projects</h1>
+          <h1 data-aos="fade-in">Professional Project</h1>
           <div className="professional-section">
             <ProfessionalPart title="Water Treatment 
   Management Dashboard" position="Front-End Developer Intern" company="Advantech" location="Taipei, Taiwan" date="July 2019 - Aug 2019" image={advantech} url="https://dashboard-adviiot-ene-showcasemplus.wise-paas.com/frame/default?orgId=5&language=en&theme=dark&refresh=5s" first="Developed user-friendly panels with platform Grafana to visualize data using 
@@ -30,11 +30,7 @@ class Professional extends Component {
            <ProfessionalPart title="DCT" company="Brilltek" position="Full Stack Developer Intern" location="Taichung, Taiwan" date="May 2019 - Aug 2019" image={dct} url="" first="Built a serverless Rest API with firebase cloud function,Express.js and Cloud 
     Firestore for mobile app" second="Cooperated with front-end team to create innovative mobile app interface and 
     features using Flutter and Dart"/>
-            <ProfessionalPart title="CannaSphere" company="CannaSphere.org and The Cannabis Fair Trade 
-          Organization" position="Jr. Front-End Dev, UI/UX Redesign Team Intern" location="Seattle, WA" date="Feb 2018 - Apr 2018" image={cannasphere} url="http://cannasphere.org/" first="Redesigned the web page layout using Adobe XD and implemented Responsive
-              Web Design using Bootstrap" second="Held daily scrum meeting with the redesign team to achieve agile development"/>
-           
-           
+                      
             <ProfessionalPart title="ISPTS 2019" company="Stevens Institute of Technology" position="Part-time Web Developer" location="Hoboken, NJ" date="Mar 2019 - June 2019" image={ispts} url="https://web.stevens.edu/ispts/index" first="Redesign the web page and translate wireframe to actual code with HTML, CSS
     ,Javascript and PHP" second=""/>
             
