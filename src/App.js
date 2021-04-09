24a4f9e514bfac425d22125f524ece376cf1468f
Wesley16838 <wesley16838@gmail.com>
2021-04-09 12:11:15 +0800

    add image

--- src/App.js
+++ src/App.js
@@ -1,54 +1,69 @@
-import React, {Component} from 'react';
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
-import MetaTags from 'react-meta-tags';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import MetaTags from "react-meta-tags";
 //includes
-import './Assets/css/styles.min.css'//css file
-import './Assets/js/scripts.min.js'//js file
+import "./Assets/css/styles.min.css"; //css file
+import "./Assets/js/scripts.min.js"; //js file
 
-import Landingpage from './landingpage';
+import Landingpage from "./landingpage";
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
-      loading:true,
-   
+      loading: true,
     };
   }
   //
   componentDidMount() {
-    this.setState({loading: false})
-}
+    this.setState({ loading: false });
+  }
   render() {
-    
     const { loading } = this.state;
 
     if (loading) {
-      return <div className="loading">
-    <svg className="spinner" width="65px" height="65px" viewBox="0 0 66 66">
-      <circle className="path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
-    </svg>
+      return (
+        <div className="loading">
+          <svg
+            className="spinner"
+            width="65px"
+            height="65px"
+            viewBox="0 0 66 66"
+          >
+            <circle
+              className="path"
+              fill="none"
+              strokeWidth="6"
+              strokeLinecap="round"
+              cx="33"
+              cy="33"
+              r="30"
+            ></circle>
+          </svg>
         </div>
+      );
     }
-    return(
-     
+    return (
       <Router>
-         <div className="App">
-         <MetaTags>
+        <div className="App">
+          <MetaTags>
             <title>Wesley Portfolio</title>
             <meta name="description" content="Wesley Portfolio" />
             <meta property="og:title" content="Wesley Portfolio" />
-            <meta property="og:image" content="./landing.png" />
-         </MetaTags>
+            <meta
+              property="og:image"
+              content="https://www.weihsuanportfolio.com/landing.png"
+            />
+          </MetaTags>
           <Switch>
-            <Route exact path="/" render={(props) => <Landingpage {...props} title="Portfolio"/>}/>
-            
+            <Route
+              exact
+              path="/"
+              render={(props) => <Landingpage {...props} title="Portfolio" />}
+            />
           </Switch>
-     
-      </div>
-      </Router>    
-      
-    )
+        </div>
+      </Router>
+    );
   }
 }
 
